505aeab9e41198318881276f2b8ef0a0e41ba87a
Ruben Ferreira <Ruben-f@outlook.com>
2026-04-19 18:20:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+string video_path = args[0];
+using (VideoCapture capture = new VideoCapture(video_path))
+{
+    using (Mat frame = new Mat())
+    {
+        int frame_index = 0;
+        int frame_interval = int.Parse(args[1]);
+
+        while (capture.Read(frame))
+        {
+            if (!frame.IsEmpty)
+            {
+                if (frame_index % frame_interval == 0)
+                {
+                    frame.Save($"frame_{frame_index}.png");
+                }
+                frame_index++;
+            }
+        }
+    }
+}
